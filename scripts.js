5010bba6f209beac76768c0c1b687bb69c6065e6
leprof <prof.violet.club@gmail.com>
2025-09-08 10:39:35 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,15 +7,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-
-// Charge le fichier Markdown et le convertit en HTML
-fetch('fanfics/histoire1.md')
-  .then(response => response.text())
-  .then(markdown => {
-    const html = marked.parse(markdown);
-    document.getElementById('content').innerHTML = html;
-  })
-  .catch(error => {
-    document.getElementById('content').innerHTML = "<p>Erreur de chargement de l'histoire.</p>";
-    console.error(error);
-  });
