e950c0fdfbd512ca8bad54012adab2195b67d648
leprof <prof.violet.club@gmail.com>
2025-09-10 13:06:54 +0200

    Update essai.js

--- essai.js
+++ essai.js
@@ -30,6 +30,9 @@ essay.forEach(fic => {
   `;
 
   zone.appendChild(card);
+  if (typeof refreshFavButtons === "function") {
+    refreshFavButtons();
+  }
 });
 
 // Tu peux réutiliser refreshFavButtons() ici si tu l’as en module
