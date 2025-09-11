3971551d32475689392cdcc9abbbe978999c2a17
leprof <prof.violet.club@gmail.com>
2025-09-11 09:11:34 +0200

    Create note.js

---
+++ note.js
@@ -0,0 +1,26 @@
+         //firebase.js
+
+// 🔥 Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+// 🔧 Configuration Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAimK0CJsMXb1EqBtfqB36hrEunO4Ybk3c",
+  authDomain: "bibliothequekassossiale.firebaseapp.com",
+  projectId: "bibliothequekassossiale",
+  storageBucket: "bibliothequekassossiale.firebasestorage.app",
+  messagingSenderId: "1029476191647",
+  appId: "1:1029476191647:web:4da50f87d9aacc635b81aa",
+  measurementId: "G-83CKLXJJHD"
+};
+
+const app = initializeApp(firebaseConfig);
+window.auth = getAuth(app);
+document.body.insertAdjacentHTML("beforeend", "<p style='color:green;'>✅ Firebase initialisé</p>");
