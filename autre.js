695ceaaef6db673e4a25233833712a3241eb40d8
leprof <prof.violet.club@gmail.com>
2025-09-17 19:05:31 +0200

    Update autre.js

--- autre.js
+++ autre.js
@@ -1,40 +1,40 @@
-const other = [
-  {
-    title: "Programme - UnFra (Version du 25 mars 2025)",
-    author: "Anthony RAVE et Mewenn RODET",
-    date: "2025-09-10",
-    type: "other",
-    description: "Analyses et théories politiques",
-    link: "essai/progunfra25mars2025.html"
-  },
-  {
-    title: "Haïku",
-    author: "Anthony RAVE",
-    date: "2025-09-10",
-    type: "other",
-    description: "Poèmes courts",
-    link: "essai/haiku.html"
-  }
-];
+//const other = [
+//  {
+//    title: "Programme - UnFra (Version du 25 mars 2025)",
+//    author: "Anthony RAVE et Mewenn RODET",
+ //   date: "2025-09-10",
+//    type: "other",
+//    description: "Analyses et théories politiques",
+//    link: "essai/progunfra25mars2025.html"
+//  },
+//  {
+//    title: "Haïku",
+//    author: "Anthony RAVE",
+ //   date: "2025-09-10",
+//    type: "other",
+//    description: "Poèmes courts",
+//    link: "essai/haiku.html"
+//  }
+//];
 
-const zone = document.getElementById("other-zone");
+//const zone = document.getElementById("other-zone");
 
-other.forEach(fic => {
-  const card = document.createElement("div");
-  card.className = "fic-card";
-  card.setAttribute("data-title", fic.title);
-  card.setAttribute("data-author", fic.author);
-  card.setAttribute("data-date", fic.date);
-  card.setAttribute("data-type", fic.type);
+//other.forEach(fic => {
+//  const card = document.createElement("div");
+//  card.className = "fic-card";
+//  card.setAttribute("data-title", fic.title);
+//  card.setAttribute("data-author", fic.author);
+//  card.setAttribute("data-date", fic.date);
+//  card.setAttribute("data-type", fic.type);
 
-  card.innerHTML = `
-    <h3>${fic.title}</h3>
-    <p><strong>Auteur :</strong> ${fic.author}</p>
-    <p><strong>Date :</strong> ${fic.date}</p>
-    <p><strong>Type :</strong> Autre</p>
-    <p>${fic.description}</p>
-    <a href="${fic.link}">Lire le Texte</a>
-  `;
+//  card.innerHTML = `
+//    <h3>${fic.title}</h3>
+ //   <p><strong>Auteur :</strong> ${fic.author}</p>
+//    <p><strong>Date :</strong> ${fic.date}</p>
+//    <p><strong>Type :</strong> Autre</p>
+//    <p>${fic.description}</p>
+//    <a href="${fic.link}">Lire le Texte</a>
+//  `;
 
-  zone.appendChild(card);
-})
+//  zone.appendChild(card);
+//})
