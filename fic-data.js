b96d086b050f74b2034b024825d9fea3c29cae0f
leprof <prof.violet.club@gmail.com>
2025-09-18 21:32:39 +0200

    Update fic-data.js

--- fic-data.js
+++ fic-data.js
@@ -62,13 +62,31 @@ export const ficCards = [
     type: "other",
     description: "w",
     link: "la_lignee_des_profs/w.html"
+  },
+  {
+    title: "1 - Le Collier d'Angela Merkel",
+    author: "Anthony RAVE",
+    date: "2025-09-17",
+    type: "other",
+    description: "Angela Merkel compte sur l'aide de son collier...",
+    link: "les_president(e)s_en_force/1_le_collier_d_Angela_Merkel.html"
+  },
+  {
+    title: "2 - La Bague d'Emmanuel Macron",
+    author: "Anthony RAVE",
+    date: "2025-09-17",
+    type: "other",
+    description: "Emmanuel Macron compte sur l'aide de sa bague...",
+    link: "les_president(e)s_en_force/2_la_bague_d_emmanuel_macron.html"
+  },
+  {
+    title: "3 - Le Pendentif d'Elizabeth II",
+    author: "Anthony RAVE",
+    date: "2025-09-17",
+    type: "other",
+    description: "Elizabeth II compte sur l'aide de son pendentif...",
+    link: "les_president(e)s_en_force/3_le_pendentif_d_Elizabeth_II.html"
   }
 ];
-
-
-
-
-
-
 
 
