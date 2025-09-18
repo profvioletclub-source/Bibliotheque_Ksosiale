d3fd30160196df61f546e08cc8a43f40db65ebce
leprof <prof.violet.club@gmail.com>
2025-09-18 22:02:26 +0200

    Update roman.js

--- roman.js
+++ roman.js
@@ -1,17 +1,9 @@
-const romans_liste = [
-  {
-    title: "Le Voyage dans le Musée",
-    author: "Collectif",
-    date: "2025-10-01",
-    type: "roman",
-    description: "En ce dimanche de janvier, Michel se réveille et se réjouit de voir le soleil dont les rayons passent à travers les volets. Mais il va vite déchanter. Il a neigé toute la nuit. Ce qui ne l'arrange pas car il doit aller à Lyon voir son amie, mais il n'a pas monté les pneus neige. Il va devoir s'y atteler, mais rien ne va se passer comme il le souhaite et il n'aura pas le temps de tout faire. Il décide alors de rester à Cran Gevrier et d'aller se promener au bord du lac d'Annecy. Mais en traversant la place Chorus, un événement va totalement bouleverser son après-midi...",
-    link: "romans/voyagemusee.html"
-  } 
-];
+import { ficCards } from './fic-data.js'
 
 const zone = document.getElementById("roman-zone");
+const filtered = ficCards.filter(fic => fic.type === "roman");
 
-romans_liste.forEach(fic => {
+filtered.forEach(fic => {
   const card = document.createElement("div");
   card.className = "fic-card";
   card.setAttribute("data-title", fic.title);
