185ba3578330b72bb8b70da1751dc040748960c3
leprof <prof.violet.club@gmail.com>
2025-10-13 15:40:37 +0200

    Update fic-data.js

--- fic-data.js
+++ fic-data.js
@@ -1,30 +1,14 @@
 export const ficCards = [
   {
-    title: "Or jaune, noir, bleu, quelle différence ?",
+    title:"Haïku",
     author: "Anthony RAVE",
-    date: "2025-09-18",
-    type: "docu",
-    description: "Quelle est la différence entre les différents types d'or ?",
-    link: "documentaire/or.html"
+    date: "2025-09-10",
+    type: "other",
+    description: "Poèmes courts",
+    link: "autre/haiku.html"
   },
   {
-    title: "Trump, le président qui tweet plus vite que son ombre",
-    author: "Anthony RAVE",
-    date: "2025-10-01",
-    type: "docu",
-    description: "Trump et Tweeter, une relation des plus intrigantes...",
-    link: "documentaire/trump.html"
-  },
-  {
-    title: "Poutine, 22 ans de règne et un pouvoir infini",
-    author: "Anthony RAVE",
-    date: "2025-10-11",
-    type: "docu",
-    description: "Au lendemain du début de l''Opération spéciale' en Ukraine, nous faisons le point",
-    link: "documentaire/poutine.html"
-  },
-  {
-    title: "Ksos 5.0 : Meurtre en Haute-Savoie",
+    title:"Ksos 5.0 : Meurtre en Haute-Savoie",
     author: "Copilot",
     date: "2025-09-09",
     type: "fanfiction",
@@ -32,21 +16,21 @@ export const ficCards = [
     link: "fanfics/fanfics1.html"
   },
   {
-    title: "Ksos 6.0 : Le Pacte des Cendres",
+    title:"Ksos 6.0 : Le Pacte des Cendres",
     author: "Copilot",
     date: "2025-09-12",
     type: "fanfiction",
     description: "Un an après le meurtre de Jules... 🔪",
     link: "fanfics/fanfics2.html"
   },
-  //{
-   // title: "Le Voyage dans le Musée",
-//    author: "Collectif",
- //   date: "2026-01-01",
-//    type: "roman",
-//    description: "En ce dimanche de janvier, Michel se réveille et se réjouit de voir le soleil dont les rayons passent à travers les volets. Mais il va vite déchanter. Il a neigé toute la nuit. Ce qui ne l'arrange pas car il doit aller à Lyon voir son amie, mais il n'a pas monté les pneus neige. Il va devoir s'y atteler, mais rien ne va se passer comme il le souhaite et il n'aura pas le temps de tout faire. Il décide alors de rester à Cran Gevrier et d'aller se promener au bord du lac d'Annecy. Mais en traversant la place Chorus, un événement va totalement bouleverser son après-midi...",
- //   link: "romans/voyagemusee.html"
-//  },
+  {
+    title:"La Lignée des Profs",
+    author: "Anthony RAVE",
+    date: "2025-09-17",
+    type: "other",
+    description: "Quelles histoires les profs ont-ils vécuent ?",
+    link: "autre/la_lignee_des_profs/1_le_fantome_des_roig.html"
+  },
   {
     title:"L'Affaire Willweston - Partie 1",
     author: "Anthony RAVE",
@@ -56,71 +40,7 @@ export const ficCards = [
     link: "romans/willweston.html"
   },
   {
-    title: "Pneumasis",
-    author: "Anthony RAVE",
-    date: "2026-01-01",
-    type: "roman",
-    description: "À venir",
-    link: "romans/pneumasis/pneumasis-ch1.html"
-  },
-  {
-    title: "Une Idylle Scolaire",
-    author: "Anthony RAVE",
-    date: "2025-10-12",
-    type: "roman",
-    description: "Quand un mauvais élève doit suivre des cours privé pour finir l'année...",
-    link: "romans/idylle.html"
-  },
-  {
-    title: "Le Monstre du Marais",
-    author: "Anthony RAVE",
-    date: "2025-10-13",
-    type: "other",
-    description: "Une Nouvelle d'Horreur...",
-    link: "autre/le_monstre_du_marais.html"
-  },
-  {
-    title: "Programme - UnFra (Version du 25 mars 2025)",
-    author: "Anthony RAVE et Mewenn RODET",
-    date: "2025-09-10",
-    type: "other",
-    description: "Analyses et théories politiques",
-    link: "autre/progunfra25mars2025.html"
-  },
-  {
-    title: "Haïku",
-    author: "Anthony RAVE",
-    date: "2025-09-10",
-    type: "other",
-    description: "Poèmes courts",
-    link: "autre/haiku.html"
-  },
-  {
-    title: "La Lignée des Profs",
-    author: "Anthony RAVE",
-    date: "2025-09-17",
-    type: "other",
-    description: "Quelles histoires les profs ont-ils vécuent ?",
-    link: "autre/la_lignee_des_profs/1_le_fantome_des_roig.html"
-  },
-  {
-    title: "Les Président(e)s en force",
-    author: "Anthony RAVE",
-    date: "2025-09-17",
-    type: "other",
-    description: "Les Président(e)s comptent sur l'aide deleurs bijoux...",
-    link: "autre/les_president(e)s_en_force/1_le_collier_d_Angela_Merkel.html"
-  },
-  {
-    title: "Un Assassin à Démotz",
-    author: "Anthony RAVE",
-    date: "2025-10-12",
-    type: "other",
-    description: "Quand un meurtre à lieu au collège...",
-    link: "autre/un_assassin_a_demotz/un_assassin_a_demotz-ch1.html"
-  },
-  {
-    title: "Le Combot des Profs",
+    title:"Le Combot des Profs",
     author: "Anthony RAVE",
     date: "2025-10-12",
     type: "other",
@@ -128,11 +48,91 @@ export const ficCards = [
     link: "autre/le_combot_des_profs/1_meurtre_a_rome.html"
   },
   {
-    title: "Le Retour des Pokemons",
+    title:"Le Monstre du Marais",
+    author: "Anthony RAVE",
+    date: "2025-10-13",
+    type: "other",
+    description: "Une Nouvelle d'Horreur...",
+    link: "autre/le_monstre_du_marais.html"
+  },
+  {
+    title:"Le Retour des Pokemons",
     author: "Anthony RAVE",
     date: "2025-10-13",
     type: "other",
     description: "Quand les Pokemons apparaissent en Haute-Savoie...",
     link: "autre/le_retour_des_pokemons/1_quoi_un_pikachu.html"
+  },
+  {
+    title:"Le Voyage dans le Musée",
+    author: "Collectif",
+    date: "2026-01-01",
+    type: "roman",
+    description: "En ce dimanche de janvier, Michel se réveille et se réjouit de voir le soleil dont les rayons passent à travers les volets. Mais il va vite déchanter. Il a neigé toute la nuit. Ce qui ne l'arrange pas car il doit aller à Lyon voir son amie, mais il n'a pas monté les pneus neige. Il va devoir s'y atteler, mais rien ne va se passer comme il le souhaite et il n'aura pas le temps de tout faire. Il décide alors de rester à Cran Gevrier et d'aller se promener au bord du lac d'Annecy. Mais en traversant la place Chorus, un événement va totalement bouleverser son après-midi...",
+    link: "romans/voyagemusee.html"
+  },
+  {
+    title:"Les Président(e)s en force",
+    author: "Anthony RAVE",
+    date: "2025-09-17",
+    type: "other",
+    description: "Les Président(e)s comptent sur l'aide deleurs bijoux...",
+    link: "autre/les_president(e)s_en_force/1_le_collier_d_Angela_Merkel.html"
+  },
+  {
+    title:"Or jaune, noir, bleu, quelle différence ?",
+    author: "Anthony RAVE",
+    date: "2025-09-18",
+    type: "docu",
+    description: "Quelle est la différence entre les différents types d'or ?",
+    link: "documentaire/or.html"
+  },
+  {
+    title:"Pneumasis",
+    author: "Anthony RAVE",
+    date: "2026-01-01",
+    type: "roman",
+    description: "À venir",
+    link: "romans/pneumasis/pneumasis-ch1.html"
+  },
+  {
+    title:"Poutine, 22 ans de règne et un pouvoir infini",
+    author: "Anthony RAVE",
+    date: "2025-10-11",
+    type: "docu",
+    description: "Au lendemain du début de l''Opération spéciale' en Ukraine, nous faisons le point",
+    link: "documentaire/poutine.html"
+  },
+  {
+    title:"Programme - UnFra (Version du 25 mars 2025)",
+    author: "Anthony RAVE et Mewenn RODET",
+    date: "2025-09-10",
+    type: "other",
+    description: "Analyses et théories politiques",
+    link: "autre/progunfra25mars2025.html"
+  },
+  {
+    title:"Trump, le président qui tweet plus vite que son ombre",
+    author: "Anthony RAVE",
+    date: "2025-10-01",
+    type: "docu",
+    description: "Trump et Tweeter, une relation des plus intrigantes...",
+    link: "documentaire/trump.html"
+  },
+  {
+    title:"Un Assassin à Démotz",
+    author: "Anthony RAVE",
+    date: "2025-10-12",
+    type: "other",
+    description: "Quand un meurtre à lieu au collège...",
+    link: "autre/un_assassin_a_demotz/un_assassin_a_demotz-ch1.html"
+  },
+  {
+    title:"Une Idylle Scolaire",
+    author: "Anthony RAVE",
+    date: "2025-10-12",
+    type: "roman",
+    description: "Quand un mauvais élève doit suivre des cours privé pour finir l'année...",
+    link: "romans/idylle.html"
   }
 ];
